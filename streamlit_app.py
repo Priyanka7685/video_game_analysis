d0048ccd0af4c1feff809b515247832c16204f49
Priyanka7685 <priyankathakur7685@gmail.com>
2025-07-19 13:50:39 +0530

    Created the streamlit_app.py and added markdown cell

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+
+df = pd.DataFrame({
+    "categories":["A","B","C","D","E"],
+    "popularity":[45,50,60,58,60]
+})
+
+print(df)
